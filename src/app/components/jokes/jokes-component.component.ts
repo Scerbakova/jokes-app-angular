718ee1aee7d7cca5db88674d28441655dcbd2d65
Scerbakova <reginadilevka@inbox.lv>
2022-06-14 15:15:38 +0300

    localstorage

--- src/app/components/jokes/jokes-component.component.ts
+++ src/app/components/jokes/jokes-component.component.ts
@@ -1,22 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Joke } from 'src/app/models/joke.model';
 
 @Component({
   selector: 'app-jokes-component',
   templateUrl: './jokes-component.component.html',
-  styleUrls: ['./jokes-component.component.scss']
+  styleUrls: ['./jokes-component.component.scss'],
 })
-export class JokesComponentComponent {
-  jokes: Joke[] = [{
-    question: "",
-    answer: "",
-  }]
+export class JokesComponentComponent implements OnInit {
+  jokes: Joke[] = [
+    {
+      question: '',
+      answer: '',
+    },
+  ];
+
+  ngOnInit() {
+    if (localStorage.getItem('Jokes') === null) {
+      this.jokes = [];
+    } else {
+      this.jokes = JSON.parse(localStorage.getItem('Jokes')!);
+    }
+  }
 
   addJoke(joke: Joke): void {
-    this.jokes = [...this.jokes, joke]
+    this.jokes = [...this.jokes, joke];
+    localStorage.setItem('Jokes', JSON.stringify(this.jokes));
   }
 
   deleteJoke(index: number): void {
     this.jokes.splice(index, 1);
+    localStorage.setItem('Jokes', JSON.stringify(this.jokes));
   }
 }
